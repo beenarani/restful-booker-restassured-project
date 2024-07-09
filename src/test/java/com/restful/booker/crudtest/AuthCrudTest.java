9864e38aff34bb0972f746fb10cc7cc770c9b54b
beenarani <ranibeena187@gmail.com>
2024-07-09 17:11:15 +0100

    My homework week 17

---
+++ src/test/java/com/restful/booker/crudtest/AuthCrudTest.java
@@ -0,0 +1,35 @@
+package com.restful.booker.crudtest;
+
+import com.restful.booker.model.AuthPojo;
+import com.restful.booker.testbase.BaseTest;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class AuthCrudTest  {
+    @BeforeClass
+    public static void inIt() {
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+        RestAssured.basePath = "/auth";
+    }
+
+    @Test
+    public void T1verifyTokenreatedSuccessfully() {
+
+        AuthPojo authPojo = new AuthPojo();
+        authPojo.setUsername("admin");
+        authPojo.setPassword("password123");
+
+        Response response =
+                given().log().all()
+                        .header("Content-Type", "application/json")
+                        .when()
+                        .body(authPojo)
+                        .post();
+        response.prettyPrint();
+        response.then().statusCode(200);
+    }
+}
